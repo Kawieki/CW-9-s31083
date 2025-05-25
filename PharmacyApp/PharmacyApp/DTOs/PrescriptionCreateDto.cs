7170ed0e3f9a1942c99f410ac553966273f4295e
s31083 <s31083@pjwstk.edu.pl>
2025-05-25 15:56:43 +0200

    update dto addonatations

--- PharmacyApp/PharmacyApp/DTOs/PrescriptionCreateDto.cs
+++ PharmacyApp/PharmacyApp/DTOs/PrescriptionCreateDto.cs
@@ -1,24 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PharmacyApp.DTOs;
 
 public class PrescriptionCreateDto
 {
+    [Required]
     public PatientDto Patient { get; set; }
+    
+    [Required]
     public DoctorDto Doctor { get; set; }
+   
+    [Required]
     public DateTime Date { get; set; }
+    
+    [Required]
     public DateTime DueDate { get; set; }
+    
+    [Required]
     public List<MedicamentDto> Medicaments { get; set; }
 }
 
 public class PatientDto
 {
+    [Required]
     public int IdPatient { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string LastName { get; set; }
+    
+    [Required]
     public DateTime Birthdate { get; set; }
 }
 
 public class DoctorDto
 {
+    [Required]
     public int IdDoctor { get; set; }
 }
 
@@ -35,7 +56,11 @@ public class MedicamentDto
         Details = details;
     }
 
+    [Required]
     public int IdMedicament { get; set; }
     public int? Dose { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string Details { get; set; }
 }
